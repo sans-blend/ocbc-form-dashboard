5a83ddd61054f03501e2e7ea0d88596f0792b846
pauwdanny9 <129724767+pauwdanny9@users.noreply.github.com>
2023-04-06 14:38:04 +0700

    Add files via upload
    v 1.1
    - checkbox dicheck memunculkan nama form
    - cara pemilihan filter date baru
    - toggle antara all submission & success submission

--- js.js
+++ js.js
@@ -73,11 +73,12 @@ const ctx = document.getElementById('myChart');
 
 
 
-$(document).ready(function() {
-    $('#example').DataTable( {
+
+   var table =  $('#form-graph').DataTable( {
         dom: 'BRfrtlip',
         buttons: [
-            'csv'
+            'csv',
+            'excel'
         ],
 
         // lengthMenu: [
@@ -85,7 +86,7 @@ $(document).ready(function() {
         //     [10, 25, 50, 'All'],
         // ],
 
-        ajax: "data.txt",
+        ajax: "data-all.txt",
         columns: [
         { data: "id"},
         { data: "name"},
@@ -94,7 +95,7 @@ $(document).ready(function() {
         { data: "start_date"}
         ]
     } );
-} );
+
 
 
 
@@ -109,3 +110,118 @@ $(document).ready(function() {
 $(function(){
   $('#datepicker-2').datepicker();
 });
+
+
+function testingFunction(){
+  alert("Hello! I am an alert box!!");
+
+
+}
+
+
+
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+  function calenderInputChange() {
+    var inputBoxStart = document.getElementById('input-start');
+    var inputBoxEnd = document.getElementById('input-end');
+    if (this.value == 'daily') {
+      inputBoxStart.type = 'date'
+      inputBoxEnd.type = 'date'
+    } else if (this.value == 'weekly') {
+      inputBoxStart.type = 'week'
+      inputBoxEnd.type = 'week'
+    } else if (this.value == 'monthly') {
+      inputBoxStart.type = 'month'
+      inputBoxEnd.type = 'month'
+    } else {
+      inputBoxStart.type = 'number'
+      inputBoxStart.value = '2022'
+      inputBoxEnd.type = 'number'
+      inputBoxEnd.value = '2023'
+    }
+  }
+
+  document.getElementById('calendar-input-type').addEventListener('change', calenderInputChange);
+
+
+
+const alertPlaceholder = document.getElementById('AlertPlaceholder')
+const appendAlert = (message, type, id) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class=" me-4 alert alert-${type} alert-dismissible d-inline-flex" role="alert" id="${id}">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+
+$(function()
+    {
+      $('#AlertCheckBox1').change(function()
+      {
+        if ($(this).is(':checked')) {
+           // Do something...
+           appendAlert(AlertCheckBox1.value, 'primary', 'test1');
+        } else {
+          const alertElement = document.getElementById("test1");
+          alertElement.remove();
+        };
+      });
+    });
+
+    $(function()
+    {
+      $('#AlertCheckBox2').change(function()
+      {
+        if ($(this).is(':checked')) {
+           // Do something...
+           appendAlert(AlertCheckBox2.value, 'primary', 'test2');
+        } else {
+          const alertElement = document.getElementById("test2");
+          alertElement.remove();
+        };
+      });
+    });
+
+
+    $(function()
+    {
+      $('#AlertCheckBox3').change(function()
+      {
+        if ($(this).is(':checked')) {
+           // Do something...
+           appendAlert(AlertCheckBox3.value, 'primary', 'test3');
+        } else {
+          const alertElement = document.getElementById("test3");
+          alertElement.remove();
+        };
+      });
+    });
+
+
+
+
+
+
+  const dataChangeTrigger1 = document.getElementById('vbtn-radio2')
+  if (dataChangeTrigger1) {
+  dataChangeTrigger1.addEventListener('click', () => {
+   table.ajax.url('/data-success.txt').load();
+  })
+}
+
+const dataChangeTrigger2 = document.getElementById('vbtn-radio1')
+  if (dataChangeTrigger2) {
+  dataChangeTrigger2.addEventListener('click', () => {
+   table.ajax.url('/data-all.txt').load();
+  })
+}
